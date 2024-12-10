7d8ebf627f83cdf59e8fc9f449cc938b0c80ad39
ShivajiRam108-github <ramudus684@gmail.com>
2024-12-10 09:41:53 +0530

    js

---
+++ script.js
@@ -0,0 +1,52 @@
+
+ 
+  // Q -1
+
+function greet (){
+    console.log('Hello!')
+}
+greet();
+
+// Q - 2
+
+function showMessage(){
+  console.log("This is amessage!")
+}    
+showMessage();
+
+ 
+
+   // Q -3
+
+   function logInfo(){
+       console.log("Loggin Information.....")
+   }
+
+logInfo();
+
+  // Q -4
+
+  function startGame(){
+   console.log("Game Started")
+   EndGame();
+
+  }
+  function EndGame(){
+   console.log("Game Over!")
+  }
+
+  startGame();
+  
+     // Q -5
+
+     function beginTask(){
+       console.log("Task Started")
+       finishTask();
+     }
+     function finishTask(){
+       console.log("Task Finished")
+     }
+
+     beginTask();
+
+    
